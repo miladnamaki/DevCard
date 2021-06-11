889b6edc9c97656029b1d2536f33b8425c470c84
miladnamaki <milad.namakii@gmail.com>
2021-06-11 22:02:23 +0430

    Project  & letsArtile move to component

--- DevCard_Mvc/Models/Article.cs
+++ DevCard_Mvc/Models/Article.cs
@@ -2,9 +2,26 @@
 {
     public class Article
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set;  }
         public string Description { get; set; }
+        public string Image { get; set;  }
+
+        public Article(int id, string title, string description, string image)
+        {
+            Id = id;
+            this.Title = title;
+            this.Description = description;
+            this.Image = image;
+        
+        }
+
+
+
+
+
+
+
 
     }
 }
---
+++ DevCard_Mvc/ViewComponet/LetsArticleViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using DevCard_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DevCard_Mvc.ViewComponet
+{
+    public class LetsArticleViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var article = new List<Article>
+            {
+                new Article(1, "آموزش Mvc Core  ", "کاملترین پکیج به زبان فارسی ", "blog-post-thumb-card-1.jpg"),
+                new Article(2,"آموزش Git&Github  ","کاملترین پکیج به زبان فارسی Git&Github  ","blog-post-thumb-card-1.jpg"),
+                new Article(2,"آموزش Onion Architecture  ","کاملترین پکیج به زبان فارسی Onion Architecture","blog-post-thumb-card-3.jpg"),
+
+
+            };
+            return View("_LatesArticles", article);
+
+        }
+    }
+}
