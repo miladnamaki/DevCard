1591d9639e73c17b6acc2c892d93c97caf3ae4ac
miladnamaki <milad.namakii@gmail.com>
2021-06-11 21:00:11 +0430

    Add componet View

--- DevCard_Mvc/Models/Project.cs
+++ DevCard_Mvc/Models/Project.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevCard_Mvc.Models
 {
     public class Project
     {
+       
+
         public int Id { get; set; }
         public string Name { get; set;  }
         public string Description { get; set;  }
+        public string Image { get; set; }
         public string Client { get; set;  }
 
+
+        public Project(int id, string name, string description,string image ,string client)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            this.Image = image;
+            Client = client;
+        }
+       
     }
+       
 }
