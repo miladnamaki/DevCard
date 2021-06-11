7b2db28f0f871e334ded1d7f6ceeff862c15c0f3
miladnamaki <milad.namakii@gmail.com>
2021-06-11 18:51:50 +0430

    Projects section from index page moved to partial view projects

---
+++ DevCard_Mvc/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace DevCard_Mvc.Models
+{
+    public class Article
+    {
+        public int id { get; set; }
+        public string Title { get; set;  }
+        public string Description { get; set; }
+
+    }
+}
